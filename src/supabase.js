b261864ac79041c1a5aa567cd6a11ec53e99c1bf
jonathandujardinpro-hub <jonathandujardinpro@gmail.com>
2026-08-31 03:42:11 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://ffxpposishqquxshfnxj.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeHBwb3Npc2hxcXV4c2hmbnhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MDk2MDAsImV4cCI6MjA2MDM4NTYwMH0.G_LSMqBQqjCgPR4RsBelDK-qdxOPOBmzeFBzA1NhVao";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeHBwb3Npc2hxcXV4c2hmbnhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMDQ5NjEsImV4cCI6MjEwMjc4MDk2MX0.Z-yti3sWGWlFikgBDRY-GLCAj2_X7ImHDeyiYbsr70A";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
